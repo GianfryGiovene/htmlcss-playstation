bffc82cdb9e0f1c77f4dd19b24b9fda7b656503c
Giovanni_Francesco_Giovene <gianfri.gvn@gmail.com>
2022-04-16 15:25:10 +0200

    start project vue cli version

---
+++ vue-cli-version/src/components/HeaderComponents/DropDownMenu.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <ul>
+            <li><a href=""><img src="@/assets/play-menu-ico.png" alt=""></a></li>
+            <li><a href=""><img src="@/assets/play-menu-ico.png" alt=""></a></li>
+        </ul>
+        <ul>
+            <li><a href="">Link1</a></li>
+            <li><a href="">Link2</a></li>
+            <li><a href="">Link3</a></li>
+            
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'DropDownMenu',
+}
+</script>
+
+<style lang='scss' scoped>
+@import '@/style/variables';
+    div{
+        color: black;
+        display: flex;
+        flex-direction: column;
+        background-color: $bg-white;
+        box-shadow: 0 0 6px;
+        position: absolute;
+        left: 0;
+        right: 0;
+        ul{
+            border-bottom: 1px solid black;
+            padding: 10px 0;
+            display: flex;
+            justify-content: center;
+            width: 100%;
+            list-style-type: $remove;
+            li{
+                padding: 0 5px;
+            }
+        }
+    }
+</style>
---
+++ vue-cli-version/src/components/MainComponent.vue
@@ -0,0 +1,35 @@
+<template>
+    <main>
+        <div id="JumboTron-container">
+            <JumboTronComponent />
+            <SectionCaroselComponent />
+        </div>
+    </main>
+</template>
+
+<script>
+import JumboTronComponent from '@/components/JumboTronComponent.vue';
+import SectionCaroselComponent from '@/components/SectionCaroselComponent.vue';
+
+export default {
+    name:'MainComponent',
+    components: {
+        JumboTronComponent,
+        SectionCaroselComponent,
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../style/general';
+
+main{
+    width: 100%;
+    height: 1000px;
+    border: 1px dashed blue;
+    #JumboTron-container{
+        width: 100%;
+    }
+}
+
+</style>
